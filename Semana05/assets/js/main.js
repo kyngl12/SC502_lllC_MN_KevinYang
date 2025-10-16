5168f7a6ed451d0eb99b4c328724bd0cd98b334c
KevinYL1 <kevinyangli123@gmail.com>
2025-10-16 14:41:39 -0600

    Semana 6 parte-2

--- Semana05/assets/js/main.js
+++ Semana05/assets/js/main.js
@@ -1,163 +1,256 @@
-saludo = "hola";
-var nombre = "Kevin Yang";
-let provincia = "Cartago";
-const pais = "Costa Rica";
+// console.log('saludo:', saludo);
+// console.log('nombre:', nombre);
+// console.log('provincia:', provincia);
+// console.log('pais:', pais);
 
-//USAREMOS LET Y CONST
+// ============================
+// VARIABLES Y TIPOS DE DATOS
+// ============================
+
+// USAREMOS LET Y CONST EN CURSO
+saludo = 'hola';
+var nombre = 'Bryan Cerdas';
+let provincia = 'Cartago';
+const pais = 'CR';
 
 provincia = [];
 provincia = 700;
 
-console.log("Saludo: ", saludo);
-console.log("Nombre: ", nombre);
-console.log("Provincia: ", provincia);
-console.log("País: ", pais);
+console.log('saludo:', saludo);
+console.log('nombre:', nombre);
+console.log('provincia:', provincia);
+console.log('pais:', pais);
 
-function sumar() {
-  console.log("Funcion sumar");
+// ============================
+// FUNCIONES
+// ============================
+
+function sumar() { 
+    console.log('funcion sumar');
 }
 
 const suma = () => {
-  console.log("Funcion suma");
-};
+    console.log('funcion suma');
+}
 
 sumar();
 suma();
 
+
+// ============================
+// EVENTO CUANDO LA PÁGINA CARGA
+// ============================
+
 document.addEventListener("DOMContentLoaded", function () {
-  const nombre = "Kevin Yang";
-  const provincia = "Cartago";
 
-  console.log("La pagina termino de cargar");
-  console.log(
-    `La pagina termino de cargar, hola ${nombre} su provincia es ${provincia}`
-  ); //Mad facil y ordenada
+    const nombre = 'Bryan Cerdas';
+    const edad = 15;
 
-  //btnCargar
-  const button = document.querySelector("#btnCargar"); //Pasamos selector CSS
+    console.log('mi pagina termino de cargar');
+    console.log("mi pagina termino de cargar " + nombre);
+    console.log(`mi pagina termino de cargar, hola ${nombre} su edad es ${edad}`);
 
-  console.log("Button: ", button);
+    const button  = document.querySelector('#btnCargar'); //Pasar selector CSS
+    // const button2  = document.getElementById('btnCargar');
 
-  //button.addEventListener('click', sumar);
-  //button.addEventListener('click', saludar);
+    console.log('button:', button);
+    // console.log('button2:', button2);
 
-  //button.addEventListener("click", cargarClientes);
+    // button.addEventListener('click', sumar);
+    // button.addEventListener('click', saludar);
 
-  button.addEventListener("click", cargarClientesHtml);
+    // button.addEventListener('click', cargarClientes);
+    button.addEventListener('click', cargarClientesHtml);
 
-  const input= document.querySelector('#input-nombre');
-  input.addEventListener('fullscreenchange', mostrarNombre);
+    const input = document.querySelector('#input-nombre');
+    input.addEventListener('change', mostrarNombre);
 
+    
 });
 
-const mostrarNombre = (event) => { //REVISAR, NO SE MUESTRA
+
+// ============================
+// FUNCIÓN PARA MOSTRAR NOMBRE
+// ============================
+// Se ejecuta cada vez que cambia el valor del input
+
+const mostrarNombre = (event) => {
 
     console.log({event});
-    
     console.log('llamando a mostrar nombre');
 
-    //const input = document.querySelector('#input-nombre');
-    const texto = document.querySelector('#texto-nombre'); //span
-
-    texto.textContent = event.target.value; //Acceder a valor del input
+    // const input = document.querySelector('#input-nombre');
+    const texto = document.querySelector('#texto-nombre'); // span
+    texto.textContent = event.target.value; // Acceder a valor del input
 }
 
 
-//crear objeto literal (n cant elementos)
-const persona = {
-  nombre: "Kevin",
-  apellido1: "Yang",
-  apellido2: "Li",
-  edad: 19,
-  direccion: "San Pedro, CR",
-  ubicacion: {
-    //Obj dentro de persona
-    provincia: "Cartago",
-    canton: "Tres Rios",
-  },
-  pasatiempos: ["Correr", "Leer", "Programar"],
-};
+// ============================
+// OBJETO LITERAL (N CANT ELEMENTOS)
+// ============================
 
-//Declaracion arreglo
-const arreglo = ["hola", 123, ["", true]];
+const persona = {
+    nombre : 'Bryan',
+    apellido1 : 'Cerdas',
+    apellido2 : 'Salas',
+    edad: 15,
+    direccion: 'San Pedro, CR',
+    ubicacion : { //Objeto dentro de persona
+        provincia : 'San Jose',
+        canton: 'Montes de Oca'
+    },
+    pasatiempos: ['Jugar Fut', 'Programar']
+}
+
+
+// ============================
+// ARREGLOS
+// ============================
+
+const arreglo = ['Hola', 25, ['', true]];
+
+
+// ============================
+// ARREGLO DE CLIENTES (OBJETOS)
+// ============================
 
 const clientes = [
-  {
-    codigo: 1,
-    nombre: "Cliente 1",
-    correo: "cliente1@gmail.com",
-    telefono: "5555-5556",
-  },
-  {
-    codigo: 2,
-    nombre: "Cliente 2",
-    correo: "cliente2@gmail.com",
-    telefono: "5555-5557",
-  },
-  {
-    codigo: 3,
-    nombre: "Cliente 3",
-    correo: "cliente3@gmail.com",
-    telefono: "5555-5558",
-  },
-  {
-    codigo: 4,
-    nombre: "Cliente 4",
-    correo: "cliente4@gmail.com",
-    telefono: "5555-5559",
-  },
-];
+    { 
+        codigo: 1, 
+        nombre: 'Cliente uno', 
+        correo: 'cliente@correo.com', 
+        telefono: '2222-4488' 
+    },
+    { codigo: 2, nombre: 'Cliente dos', correo: 'cliente@correo.com', telefono: '2222-4488' },
+    { codigo: 3, nombre: 'Cliente tres', correo: 'cliente@correo.com', telefono: '2222-4488' },
+    { codigo: 4, nombre: 'Cliente cuatro', correo: 'cliente@correo.com', telefono: '2222-4488' },
+    { codigo: 5, nombre: 'Cliente cinco', correo: 'cliente@correo.com', telefono: '2222-4488' },
+]
 
-const tbody = document.querySelector('#tbody-section'); //Aca necesito crear las tablas, le doy un id
+// ============================
+// SELECCIÓN Y LIMPIEZA DEL TBODY
+// ============================
 
-tbody.innerHTML = ''; //Limpiar el tbody al cargar pagina
+const tbody = document.querySelector('#tbody-section');
+tbody.innerHTML = ''; // Limpiar tabla antes de cargar datos
+
+
+// ============================
+// FUNCIÓN PARA CARGAR CLIENTES (CREANDO ELEMENTOS)
+// ============================
 
 const cargarClientes = () => {
-  console.log("Llamando funcion cargarClientes");
+    console.log('llamando a mi funcion cargar clientes');
 
-  //console.log(clientes);
+    // console.log(clientes);
 
-  clientes.forEach(cliente => {  //Recorrer cada elemento 
-    const fila = document.createElement('tr'); //Crear fila
+    clientes.forEach(cliente => {
 
-    const celdaCodigo = document.createElement('td');
-    celdaCodigo.textContent = cliente.codigo; //Asignar valor
+        const fila  = document.createElement('tr'); // Crear fila
 
-    const celdaNombre = document.createElement('td');
-    celdaNombre.textContent = cliente.nombre;
+        const celdaCodigo = document.createElement('td');
+        celdaCodigo.textContent = cliente.codigo; // Asignar valor
 
-    const celdaCorreo = document.createElement('td');
-    celdaCorreo.textContent = cliente.correo;
+        const celdaNombre = document.createElement('td');
+        celdaNombre.textContent = cliente.nombre;
 
-    const celdaTelefono = document.createElement('td');
-    celdaTelefono.textContent = cliente.telefono;
+        const celdaCorreo = document.createElement('td');
+        celdaCorreo.textContent = cliente.correo;
 
-    fila.appendChild(celdaCodigo); //Agregar celda a la fila
-    fila.appendChild(celdaNombre);
-    fila.appendChild(celdaCorreo);
-    fila.appendChild(celdaTelefono);
+        const celdaTelefono = document.createElement('td');
+        celdaTelefono.textContent = cliente.telefono;
 
-    tbody.appendChild(fila); //Agregar fila al tbody
-  });
-};
+        fila.appendChild(celdaCodigo); // Agregar celda a la fila
+        fila.appendChild(celdaNombre);
+        fila.appendChild(celdaCorreo);
+        fila.appendChild(celdaTelefono);
+
+        tbody.appendChild(fila); // Agregar fila al tbody
+    });
+
+}
+
+
+// ============================
+// FUNCIÓN PARA CARGAR CLIENTES CON HTML DIRECTO
+// ============================
 
 const cargarClientesHtml = () => {
 
-    let fila = '';
-    
-    clientes.forEach(cliente => {
-    
-         fila += `<tr>   
-                <td>${cliente.codigo}</td>    
-                <td>${cliente.nombre}</td>
-                <td>${cliente.correo}</td>
-                <td>${cliente.telefono}</td>               
-              </tr> `;
-})
-    tbody.innerHTML = fila; //Agregar todas las filas de una vez al contenido HTML
-}; 
+    let filas = ''; // Variable para almacenar filas en formato HTML
 
+    clientes.forEach(cliente => {
+
+        filas += `<tr>
+                      <td> <input type="checkbox" class="chkCliente" data-id="${cliente.codigo}"></td>
+                      <td>${cliente.codigo}</td>
+                      <td>${cliente.nombre}</td>
+                      <td>${cliente.correo}</td>
+                      <td>${cliente.telefono}</td>
+                  </tr>`;
+
+    });
+
+    tbody.innerHTML = filas; // Insertar las filas generadas en el tbody > Aqui existen elementos, los creo
+
+    document.querySelectorAll('.chkCliente').forEach(chk => {  //Obtener todos elementos del checkbox (evento,funcion), setear evento a elementos
+    chk.addEventListener('click', handleCheckCliente); // Asigna evento a cada checkbox individual
+    });
+
+}
+
+
+// ============================
+// FUNCIÓN SALUDAR
+// ============================
 
 const saludar = () => {
-  console.log("Hola desde saludar");
+    console.log('Hola, bienvenido!');
+}
+
+  // ============================
+// FUNCIÓN mostrarTarjetaCliente
+// ============================
+
+const mostrarTarjetaCliente = (cliente) => {
+
+    const tarjeta = document.querySelector('.card');
+
+    const titulo = tarjeta.querySelector('.card-title'); 
+    const parrafo = tarjeta.querySelector('.card-text'); 
+
+    titulo.textContent = cliente.codigo + ' - ' + cliente.nombre;
+    parrafo.textContent = 'Teléfono: ' + cliente.telefono;        
 };
+
+// ============================
+// FUNCIÓN handleCheckCliente
+// ============================
+
+const handleCheckCliente = (event) => {
+  console.log('handle check cliente');
+  console.log(event);
+
+  const checkbox = event.target; //Accesar a target del elemento
+  const codigoCliente = checkbox.dataset.id; //Obtener ID
+
+  console.log({codigoCliente});
+
+  const cliente = clientes.find(c => c.codigo == codigoCliente); //Buscar dentro del arreglo cliente
+  
+  if(cliente) {
+    console.log(cliente);
+
+    mostrarTarjetaCliente(cliente);
+
+  } else {
+    console.log('El cliente no se encuentra');
+  }
+
+
+
+
+
+}
+
+
