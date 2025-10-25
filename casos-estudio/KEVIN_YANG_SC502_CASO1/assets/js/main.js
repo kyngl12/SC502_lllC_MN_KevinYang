2947d2e4f023805a0cfdcbc4fa299f46d799be33
KevinYL1 <kevinyangli123@gmail.com>
2025-10-24 21:19:10 -0600

    Caso 1 borrador 2

---
+++ casos-estudio/KEVIN_YANG_SC502_CASO1/assets/js/main.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btnEnviar = document.querySelector("#btnEnviar");
+    btnEnviar.addEventListener("click", validarDatos);
+
+});
+
+const validarDatos = () => {
+    const nombre = document.querySelector("#nombre").value;
+    const apellidos = document.querySelector("#apellidos").value;
+    const nota = document.querySelector("#nota").value;
+
+    if (nombre == "") {
+        Swal.fire({
+            title: "Error al enviar el nombre",
+            text: "Por favor, escribe tu nombre en formato de texto",
+            icon: "error",
+            confirmButtonText: "Aceptar",
+        });
+    } else if (apellidos == "") {
+        Swal.fire({
+            title: "Error al enviar los apellidos",
+            text: "Por favor, Por favor, escribe tus apellidos en formato de texto",
+            icon: "error",
+            confirmButtonText: "Aceptar",
+        });
+    } else if (nota == "") {
+        Swal.fire({
+            title: "Error al enviar la nota",
+            text: "Por favor, ingresa un valor numérico",
+            icon: "error",
+            confirmButtonText: "Aceptar",
+        });
+    } else if (nota < 0 || nota > 100) {
+        Swal.fire({
+            title: "Error al enviar la nota",
+            text: "Sólo se permiten valores entre 0 y 100",
+            icon: "warning",
+            confirmButtonText: "Aceptar",
+        });
+    } else {
+        Swal.fire({
+            title: "Enviando...",
+            text: "Datos enviados correctamente",
+            icon: "success",
+            confirmButtonText: "Aceptar",
+        });
+    }
+};
